a33fcb32062796215fb93283dafa68f0f7c8f837
Liam Tiernan <ltiernan@mmitnetwork.com>
2023-08-16 19:49:20 -0400

    Summary component stuffs

--- frontend/src/features/portal-form/PurchaseViewerLayout.tsx
+++ frontend/src/features/portal-form/PurchaseViewerLayout.tsx
@@ -1,5 +1,6 @@
 import {
   Container,
+  Divider,
   Flex,
   Paper,
   Stack,
@@ -8,73 +9,62 @@ import {
   Transition,
 } from "@mantine/core";
 import { LoginInfo } from "./LoginInfo";
-import { ReactElement, useState } from "react";
 import { CampaignSummary } from "./fields/CampaignGoals";
 import { AdDesignQuestions } from "./fields/AdDesignQuestions";
 import { DemographicQuestions } from "./fields/DemographicQuestions";
 import { SpendSlider } from "./fields/BudgetOptions";
 import { ContactInfo } from "./ContactInfo";
 import { PaymentInfo } from "./PaymentInfo";
-
-// function FormComponentTransition({ children, isActive }: { children: ReactElement, isActive: boolean }) {
-//   return (
-//     <Transition
-//       mounted={isActive}
-//       transition="scale-y"
-//       duration={200}
-//       onExit={}
-//     >
-//       {(styles) => <div style={styles}>{children}</div>}
-//     </Transition>
-//   );
-// }
-
-function useTransitionManager() {
-  const [isExiting, setIsExiting] = useState(false);
-
-  const onEnter = () => {
-    if (isExiting) {
-    }
-  };
-
-  return;
-}
+import { useAdPurchaseFormContext } from "./form-context";
 
 function SummaryViewer({ styles }: { styles: React.CSSProperties }) {
+  const { getInputProps } = useAdPurchaseFormContext();
+  const monthlyCost = getInputProps("target_monthly_spend").value;
   return (
-    <Paper withBorder p="md" h="200px" style={{ flexBasis: "40%", ...styles }}>
-      <Title>Summary</Title>
+    <Paper withBorder p="md" h="100%" style={{ flexBasis: "30%", ...styles }}>
       <Stack>
+        <Title fw={400}>Ad Summary</Title>
+        <Divider />
         <Flex justify={"space-between"}>
-          <Text fw={600}>Estimated Reach:</Text>
-          <Text>80,000</Text>
-        </Flex>
-        <Flex justify={"space-between"}>
-          <Text fw={600}>Impact Score:</Text>
-          <Text>9.5</Text>
-        </Flex>
-        <Flex justify={"space-between"}>
-          <Text fw={600} align="left">
-            Monthly Cost:
+          <Text size="lg" align="left">
+            Monthly Cost
           </Text>
-          <Text>$500</Text>
+          <Text size="lg">${monthlyCost}</Text>
         </Flex>
+        <Divider />
+        <Flex justify={"space-between"}>
+          <Text size="lg">Estimated Reach</Text>
+          <Text fw={600} size="lg" color="brandBlue">
+            80,000
+          </Text>
+        </Flex>
+        <Flex justify={"space-between"}>
+          <Text size="lg">Impact Score</Text>
+          <Text size="lg" fw={600} color="brandBlue">
+            9.5 / 10
+          </Text>
+        </Flex>
+        <Divider />
       </Stack>
     </Paper>
   );
 }
 
 export function PurchaseViewerLayout({ activeStep }: { activeStep: number }) {
+  const steps = [
+    <LoginInfo />,
+    <CampaignSummary />,
+    <AdDesignQuestions />,
+    <DemographicQuestions />,
+    <SpendSlider />,
+    <ContactInfo />,
+    <PaymentInfo />,
+  ];
+
   return (
     <Container size="xl" mt="lg">
       <Flex justify={"space-between"}>
-        <LoginInfo />
-        <CampaignSummary />
-        <AdDesignQuestions />
-        <DemographicQuestions />
-        <SpendSlider />
-        <ContactInfo />
-        <PaymentInfo />
+        {steps[activeStep]}
         <Transition
           mounted={activeStep > 2}
           transition="fade"
