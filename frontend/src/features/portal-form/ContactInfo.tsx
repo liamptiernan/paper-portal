b6ba7a4c2d82ffe9540ee33498639b1705d087dc
Liam Tiernan <ltiernan@mmitnetwork.com>
2024-04-28 13:17:00 -0400

    some form validation

--- frontend/src/features/portal-form/ContactInfo.tsx
+++ frontend/src/features/portal-form/ContactInfo.tsx
@@ -1,4 +1,4 @@
-import { Flex, Select, TextInput, Title } from "@mantine/core";
+import { Flex, Select, Space, TextInput, Text, Title } from "@mantine/core";
 import { useAdPurchaseFormContext } from "./form-context";
 import { FormSection } from "../../components/FormSection";
 export function ContactInfo() {
@@ -7,22 +7,38 @@ export function ContactInfo() {
     { value: "ny", label: "NY" },
     { value: "pa", label: "PA" },
   ];
+
+  const SectionTitle = (
+    <Title fw={400}>
+      <Text span inherit>
+        Your Contact Info
+      </Text>
+      <Space h="md" />
+      <Text inherit fw={400} fz={"xl"} color="brandDark.2">
+        We'll use this to contact you about your ad.
+      </Text>
+    </Title>
+  );
+
   return (
-    <FormSection title={<Title fw={400}>Your contact info</Title>}>
+    <FormSection title={SectionTitle}>
       <TextInput
         label="Your Name"
         size="md"
+        required
         {...getInputProps("contact_name")}
       />
       <TextInput
         type="tel"
         label="Phone Number"
         size="md"
+        required
         {...getInputProps("contact_phone")}
       />
       <TextInput
         label="Address Line 1"
         size="md"
+        required
         {...getInputProps("address_1")}
       />
       <TextInput
@@ -31,15 +47,26 @@ export function ContactInfo() {
         {...getInputProps("address_2")}
       />
       <Flex gap={"md"}>
-        <TextInput label="City" size="md" {...getInputProps("address_city")} />
+        <TextInput
+          label="City"
+          size="md"
+          required
+          {...getInputProps("address_city")}
+        />
         <Select
           data={options}
           label="State"
           size="md"
+          required
           {...getInputProps("address_state")}
         />
       </Flex>
-      <TextInput label="Zip" size="md" {...getInputProps("address_zip")} />
+      <TextInput
+        label="Zip"
+        size="md"
+        required
+        {...getInputProps("address_zip")}
+      />
     </FormSection>
   );
 }
